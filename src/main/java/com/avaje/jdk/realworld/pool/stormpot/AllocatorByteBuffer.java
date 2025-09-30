7917bc4c0c5758cc92e75845e99388747f382e5f
donnie <6776930+donniexyz@users.noreply.github.com>
2025-09-30 14:39:24 +0800

    Allow Little Endian AllocatorByteBuffer

--- src/main/java/com/avaje/jdk/realworld/pool/stormpot/AllocatorByteBuffer.java
+++ src/main/java/com/avaje/jdk/realworld/pool/stormpot/AllocatorByteBuffer.java
@@ -1,19 +1,23 @@
 package com.avaje.jdk.realworld.pool.stormpot;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import stormpot.Reallocator;
 import stormpot.Slot;
 
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
+@RequiredArgsConstructor
 @AllArgsConstructor
 public class AllocatorByteBuffer implements Reallocator<PollableByteBuffer> {
 
     private final int bufferSize;
+    private boolean littleEndian;
 
     @Override
     public PollableByteBuffer allocate(Slot slot) {
-        return new PollableByteBuffer(slot, ByteBuffer.allocateDirect(bufferSize));
+        return new PollableByteBuffer(slot, ByteBuffer.allocateDirect(bufferSize).order(littleEndian ? ByteOrder.LITTLE_ENDIAN : ByteOrder.BIG_ENDIAN));
     }
 
     @Override
--- src/main/java/com/avaje/jdk/realworld/service/DirectByteBufferFactory.java
+++ src/main/java/com/avaje/jdk/realworld/service/DirectByteBufferFactory.java
@@ -1,16 +1,22 @@
 package com.avaje.jdk.realworld.service;
 
 import com.google.flatbuffers.FlatBufferBuilder;
+import lombok.extern.slf4j.Slf4j;
 
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.concurrent.atomic.AtomicInteger;
 
+@Slf4j
 public class DirectByteBufferFactory extends FlatBufferBuilder.ByteBufferFactory {
 
     public static final DirectByteBufferFactory INSTANCE = new DirectByteBufferFactory();
 
+    private static final AtomicInteger cnt = new AtomicInteger();
+
     @Override
     public ByteBuffer newByteBuffer(int capacity) {
+        if (cnt.incrementAndGet() % 100 == 0) log.info("capacity: {}", capacity);
         return ByteBuffer.allocateDirect(capacity).order(ByteOrder.LITTLE_ENDIAN);
     }
 }
